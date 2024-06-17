fe10331c27e3665b71b0073fb56ff27b7ded61c0
TigorBekerja <120919621+TigorBekerja@users.noreply.github.com>
2024-06-17 13:32:59 +0700

    dibagi bagi katanya, jadi bisa di cek per kata
    bug:
    gabisa meriksa struktur yang ada spasi, kebanyakan di struktur keterangan ("di malam hari", "di lapangan")
    "aku aku memotong" = SSP, harusnya S nya satu, gatau sih remed b indo slurr

--- tubestba.go
+++ tubestba.go
@@ -2,23 +2,44 @@ package main
 
 import "fmt"
 
-type arrByte [5]byte
-
 func main() {
 	var kalimat string
-	bacaSubjek(&kalimat)
-	bacaPredikat(&kalimat)
-	fmt.Println(kalimat)
+	var struktur string
+	var check string
+	var i int
+	var j int = -1
+	bacaKalimat(&kalimat)
+	for i < len(kalimat) {
+		for j = i; j < len(kalimat) && kalimat[j] != ' '; j++ {
+			fmt.Println(i, j)
+			check = check + string(rune(kalimat[j]))
+		}
+		fmt.Println(check)
+		bacaSubjek(check, &struktur)
+		bacaPredikat(check, &struktur)
+		bacaObjek(check, &struktur)
+		bacaKeterangan(check, &struktur)
+		i = j + 1
+		check = ""
+	}
+	fmt.Println(struktur)
 }
 
-//SUBJEK
-func bacaSubjek(kalimat *string) {
+func bacaKalimat(kalimat *string) {
+	var input byte
+	fmt.Scanf("%c", &input)
+	for input != '\r' {
+		*kalimat = *kalimat + string(rune(input))
+		fmt.Scanf("%c", &input)
+	}
+}
+
+func bacaSubjek(kalimat string, struktur *string) {
 	var statement byte
 	var point int
 	var error bool = false
-	fmt.Scanf("%c", &statement)
-	fmt.Println("SUBJEK")
-	for statement != ' ' {
+	for i := 0; i < len(kalimat); i++ {
+		statement = kalimat[i]
 		if point == 0 {
 			if statement == 'a' {
 				point = 1
@@ -80,22 +101,18 @@ func bacaSubjek(kalimat *string) {
 		} else {
 			break
 		}
-		//*kalimat = *kalimat + string(rune(statement)) //masukin satu satu ke string
-		fmt.Scanf("%c", &statement)
 	}
 	if (point == 13 || point == 14) && error == false {
-		*kalimat = *kalimat + "S"
+		*struktur = *struktur + "S"
 	}
 }
 
-//PREDIKAT
-func bacaPredikat(kalimat *string) {
+func bacaPredikat(kalimat string, struktur *string) {
 	var statement byte
 	var point int
 	var error bool = false
-	fmt.Println("PREDIKAT")
-	fmt.Scanf("%c", &statement)
-	for statement != ' ' {
+	for i := 0; i < len(kalimat); i++ {
+		statement = kalimat[i]
 		if point == 0 && statement == 'm' {
 			point = 1
 		} else if point == 1 && statement == 'e' {
@@ -179,10 +196,178 @@ func bacaPredikat(kalimat *string) {
 		} else {
 			break
 		}
-		//*kalimat = *kalimat + string(rune(statement)) //masukin satu satu ke string
-		fmt.Scanf("%c", &statement)
 	}
 	if (point == 22 || point == 23) && error == false {
-		*kalimat = *kalimat + "P"
+		*struktur = *struktur + "P"
+	}
+}
+
+func bacaObjek(kalimat string, struktur *string) {
+	var statement byte
+	var point int
+	var error bool = false
+	for i := 0; i < len(kalimat); i++ {
+		statement = kalimat[i]
+		if point == 0 {
+			if statement == 'k' {
+				point = 1
+			} else if statement == 'b' {
+				point = 2
+			} else if statement == 'p' {
+				point = 3
+			} else {
+				break
+			}
+		} else if point == 1 && statement == 'a' {
+			point = 4
+		} else if point == 2 && statement == 'e' {
+			point = 5
+		} else if point == 3 && statement == 'a' {
+			point = 6
+		} else if point == 4 && statement == 'y' {
+			point = 7
+		} else if point == 5 {
+			if statement == 's' {
+				point = 8
+			} else if statement == 'n' {
+				point = 9
+			} else {
+				break
+			}
+		} else if point == 6 {
+			if statement == 'p' {
+				point = 10
+			} else if statement == 't' {
+				point = 11
+			} else {
+				break
+			}
+		} else if point == 7 && statement == 'u' {
+			point = 18
+		} else if point == 8 && statement == 'i' {
+			point = 18
+		} else if point == 9 && statement == 'd' {
+			point = 12
+		} else if point == 10 && statement == 'a' {
+			point = 13
+		} else if point == 11 && statement == 'u' {
+			point = 14
+		} else if point == 12 && statement == 'e' {
+			point = 15
+		} else if point == 13 && statement == 'n' {
+			point = 18
+		} else if point == 14 && statement == 'n' {
+			point = 16
+		} else if point == 15 && statement == 'r' {
+			point = 17
+		} else if point == 16 && statement == 'g' {
+			point = 18
+		} else if point == 17 && statement == 'a' {
+			point = 18
+		} else if point == 18 {
+			if statement == ' ' {
+				point = 19
+			} else {
+				error = true
+				break
+			}
+		} else if point == 19 {
+			if statement == ' ' {
+				point = 19
+			} else if statement == 'k' {
+				point = 1
+			} else if statement == 'b' {
+				point = 2
+			} else if statement == 'p' {
+				point = 3
+			} else {
+				error = true
+				break
+			}
+		} else {
+			break
+		}
+	}
+	if (point == 18 || point == 19) && error == false {
+		*struktur = *struktur + "O"
+	}
+}
+
+func bacaKeterangan(kalimat string, struktur *string) {
+	var statement byte
+	var point int
+	var error bool = false
+	for i := 0; i < len(kalimat); i++ {
+		statement = kalimat[i]
+		if point == 0 {
+			if statement == 'p' {
+				point = 1
+			} else if statement == 'd' {
+				point = 2
+			}else{
+				break
+			}
+		}else if point == 1 && statement == 'a' {
+			point = 3
+		}else if point == 2 && statement == 'i' {
+			point = 4
+		}else if point == 3 && statement == 'd' {
+			point = 5
+		}else if point == 4 && statement == ' ' {
+			point = 6
+		}else if point == 5 && statement == 'a' {
+			point = 7
+		}else if point == 6 {
+			if statement == 'r' {
+				point = 8
+			}else if statement == 'l' {
+				point = 9
+			}else if statement == 'h' {
+				point = 10
+			}else{
+				break
+			}
+		}else if point == 7 && statement == ' ' {
+			point = 11
+		}else if point == 8 && statement == 'u' {
+			point = 12
+		}else if point == 9 && statement == 'a' {
+			point = 13
+		}else if point == 10 && statement == 'a' {
+			point = 14
+		}else if point == 11 && statement == 'p' {
+			point = 24
+		}else if point == 12 && statement == 'm' {
+			point = 15
+		}else if point == 13 && statement == 'p' {
+			point = 16
+		}else if point == 14 && statement == 'l' {
+			point = 17
+		}else if point == 15 && statement == 'a' {
+			point = 18
+		}else if point == 16 && statement == 'a' {
+			point = 19
+		}else if point == 17 && statement == 'a' {
+			point = 20
+		}else if point == 18 && statement == 'h'{
+			point = 36
+		}else if point == 19 && statement == 'n' {
+			point = 21
+		}else if point == 20 && statement == 'm' {
+			point = 22
+		}else if point == 21 && statement == 'g' {
+			point = 22
+		}else if point == 22 && statement == 'a' {
+			point = 23
+		}else if point == 23 && statement == 'n' {
+			point = 36
+		}else if point == 24 && statement == 'a' {
+			point = 26
+		}else if point == 25 && statement == 'a' {
+			point = 27
+		}else if point == 26 && statement =
+	}
+	if (point == 36 || point == 37) && error == false {
+		*struktur = *struktur + "O"
 	}
 }
